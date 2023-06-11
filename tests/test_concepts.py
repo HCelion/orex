1da42e2421211b6fbb78e1d0624b5192a14c8d5b
Arvid J. Kingl <arvid@plutodata.co.uk>
2023-06-11 22:00:31 +0100

    added some basic tests

--- tests/test_concepts.py
+++ tests/test_concepts.py
@@ -10,8 +10,7 @@ def test_constants_can_be_found():
 
     s = "foo123bar"
 
-    orex = Orex()
-    pattern = orex.DIGIT.DIGIT.DIGIT.compile()
+    pattern = Orex().DIGIT.DIGIT.DIGIT.compile()
 
     result = re.search(pattern, s)
     assert result is not None
@@ -19,3 +18,16 @@ def test_constants_can_be_found():
     results = re.findall(pattern, s)
     assert len(results) == 1
     assert results[0] == "123"
+
+
+def test_starts_with():
+
+    s = "foo123bar"
+
+    pattern = Orex().starts_with("f").compile()
+    result = re.search(pattern, s)
+    assert result is not None
+
+    pattern2 = Orex().starts_with("b").compile()
+    result = re.search(pattern2, s)
+    assert result is None
